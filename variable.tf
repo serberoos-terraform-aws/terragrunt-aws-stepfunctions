1bc54ee5cdb7dab2b8f08fc827165a02de71574a
serberoos <jae99c@gmail.com>
2025-07-30 15:24:48 +0900

    batch: 모듈 표준화 개선 완료 - 2025-07-30 15:19:36

--- variable.tf
+++ variable.tf
@@ -1,17 +1,14 @@
 variable "stepfunctions_state_machine_create" {
-  description = "Defined stepfunctions_state_machine_create Information"
   type        = map(any)
   default     = {}
 }
 
 variable "stepfunctions_activity_create" {
-  description = "Defined stepfunctions_activity_create Information"
   type        = map(any)
   default     = {}
 }
 
 variable "stepfunctions_alias_create" {
-  description = "Defined stepfunctions_alias_create Information"
   type        = map(any)
   default     = {}
 } 
